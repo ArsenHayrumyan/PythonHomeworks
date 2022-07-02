80b28c962fe071e245e0d92638f459ce9b13d841
ArsenHayrumyan <arsen.hayrumyan@gmail.com>
2022-07-02 11:48:28 +0400

    Errors

---
+++ lesson20.py
@@ -0,0 +1,33 @@
+# import calc
+
+# while True:
+#     try:
+#         print(calc.plus(int(input('Enter number: ')), int(input('Enter number: ')), int(input('Enter number: '))))
+#         break
+#     except ValueError:
+#         print('You entered the wrong character. Please enter numbers again.')
+
+# while True:
+#     try:
+#         print(calc.mult(int(input('Enter number: ')), int(input('Enter number: ')), int(input('Enter number: '))))
+#         break
+#     except ValueError:
+#         print('You entered the wrong character. Please enter numbers again.')
+    
+# while True:
+#     try:
+#         print(calc.minus(int(input('Enter number: ')), int(input('Enter number: '))))
+#         break
+#     except ValueError:
+#         print('You entered the wrong character. Please enter numbers again.')
+
+        
+# while True:
+#     try:
+#         print(calc.baj(int(input('Enter number: ')), int(input('Enter number: '))))
+#         break
+#     except ValueError:
+#         print('You entered the wrong character. Please enter numbers again.')
+#     except ZeroDivisionError:
+#         print('You cannot divide by zero. Please enter numbers again.')
+
