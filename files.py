8f2aa9e3afd3146f7dbbb41be83fdb6a37a9fab0
ArsenHayrumyan <arsen.hayrumyan@gmail.com>
2022-07-14 19:19:57 +0400

    Updated 14.07.2022

--- files.py
+++ files.py
@@ -75,6 +75,58 @@
 #     for k in range(1, len(newlist)):
 #         if len(newlist[k]) == len(newlist[0]):
 #             print(f'The next longest word in text with {len(newlist[0])} characters is \"{newlist[k]}\"') 
+
+# ----------------------------------------------
+
+# '''154. Частота букв в файле'''
+
+# def char(a:str):
+#     count = 0
+#     with open('longest_word.txt', 'rt') as x:
+#         res = x.read()
+#         for i in res:
+#             if i.lower() == a.lower():
+#                 count += 1
+#         return count
+            
+        
+# letter = input('Enter letter to count: ')
+# while len(letter) > 1 or letter.isdigit():
+#     print('You must enter only a letter.')
+#     letter = input('Please, try again: ')
+# print(f'There are {char(letter)} \"{letter}\" in text.')
+
+# ----------------------------------------------
+
+# '''155. Частота слов в файле'''
+
+# def word(s:str):
+#     with open('longest_word.txt', 'rt') as f:
+#         res = f.readlines()
+#         word = ''
+#         mylist = []
+#         for i in res:
+#             i = i.replace('\n', '')
+#             for j in i:
+#                     word += j
+#                     mylist.append(word)
+#                     word = ''
+#         str = ''.join(mylist)
+#         newlist = str.split(' ')
+#         count = 0
+#         for i in newlist:
+#             if s.lower() == i.lower():
+#                 count += 1
+#         return count
+
+# myword = input('Enter word to count: ')
+# while len(myword) < 1 or myword.isdigit() or ' ' in myword:
+#     print('You must enter only a word.')
+#     myword = input('Please, try again: ')
+# print(f'There are {word(myword)} \"{myword}\" in text.')
+
+
+
             
     
 
